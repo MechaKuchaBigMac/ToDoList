b3b5a6cb9d65e390f1ac2db6121624b048033abd
MechaKuchaBigMac <xtcbrazil@outlook.com>
2026-06-10 10:24:46 +0300

    All Done

--- pckgs/options.py
+++ pckgs/options.py
@@ -5,8 +5,11 @@ def AddTask(filePath: str,title: str):
     with open(filePath, "r", encoding="utf-8") as f:
         db = json.load(f)
 
+        status = 'X'
+
         task = {
             "id":         db["next_id"],
+            "status":     status,
             "title":      title,
             "created_at": datetime.now().strftime("%Y-%m-%d %H:%M")
         }
@@ -16,5 +19,69 @@ def AddTask(filePath: str,title: str):
 
         with open(filePath, "w", encoding="utf-8") as f:
             json.dump(db,f, indent=4, ensure_ascii=False)
+
+            print(f"Task \"{title}\" successfuly added to ({filePath})")
+
             return task
     
+    return
+
+def ShowTasks(filePath:str):
+    with open(filePath, 'r', encoding="utf-8") as fl:
+        db = json.load(fl)
+
+        tasks = db.get("tasks",[])
+        
+        if not tasks:
+            print("\n [WARNING] There's no single task left")
+            return
+
+        print("\n" + "~"*55)
+        print(f"{"St":<2} | {"ID":<5} | {"Task Name":<20} | {"Created at"}")
+        print("="*55)
+        for ts in tasks:
+            print(f"{ts["status"]:<2} | {ts["id"]:<5} | {ts["title"]:<20} | {ts["created_at"]}")
+        print("~"*55 + "\n")
+
+        
+    
+    return
+
+def Compleate(filePath:str, taskID:int):
+    with open(filePath, 'r', encoding="utf-8") as ffff:
+        db = json.load(ffff)
+
+        taskFound = False
+
+        for task in db['tasks']:
+            if task["id"] == taskID:
+                task["status"] = "O"
+                taskFound      = True
+                break
+        
+        if not taskFound:
+            print(f"\nThere's no task with ID:{taskID}")
+    
+    with open(filePath, 'w', encoding="utf-8") as f:
+        json.dump(db, f, indent=4, ensure_ascii=False)
+    
+    print(f"\nTask with ID {taskID} successfuly updated!")
+        
+    return
+
+def DeleteTask(filePath:str,taskID:int):
+    with open(filePath, 'r', encoding="utf-8") as f:
+        db = json.load(f)
+    
+    initial_count = len(db["tasks"])
+
+    db["tasks"] = [task for task in db["tasks"] if task["id"] != taskID]
+
+    if len(db["tasks"]) == initial_count:
+        print(f"\nThe task with ID: {taskID} is not Found")
+        return False
+    
+    with open(filePath,'w', encoding="utf-8") as f:
+        json.dump(db, f, indent=4,ensure_ascii=False)
+    print(f"The task with ID: {taskID} successfuly deleted")
+    return True
--- to_do_list.py
+++ to_do_list.py
@@ -1,6 +1,6 @@
 import pckgs.options as opt
 
-filePath = "README.md"
+filePath = "D:\INQUISITOR\ToDoList\db.json"
 
 def main():
     isAlive = True
@@ -9,26 +9,28 @@ def main():
         command = input("(a)[add] (s)[show] (c)[compleate] (d)[delete] (e)[exit]\nChoose the option: ")
 
         if command == "add" or command == "a":
-            taskName = input("The task: ")
+            taskName = str(input("The task: "))
             opt.AddTask(filePath, taskName)
             continue
 
-        if command == "show":
-            pass
+        if command == "show" or command == "s":
+            opt.ShowTasks(filePath)
+            continue
 
-        if command == "compleate":
-            pass
+        if command == "compleate" or command == "c":
+            taskID = int(input("(COMPLEATE)Task ID: "))
+            opt.Compleate(filePath,taskID)
+            continue
 
-        if command == "delete":
-            pass
+        if command == "delete" or command == "d":
+            taskID = int(input("(DELETE)Task ID: "))
+            opt.DeleteTask(filePath,taskID)
+            continue
 
         if command == "exit" or command == "e":
+            print("You're pressed 'exit' bye bye..(^ w ^)")
             isAlive = False
     
-    
-    
-    
-    
-    return
+    return 0;
 
 main()
